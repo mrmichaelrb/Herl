ba43ed4823375edfbe636c03c08ffc41af0eb772
mrmichaelrb <mrmichaelrb@gmail.com>
2021-09-22 03:35:56 -0600

    Git compatibility improvements

---
+++ Herl/Assets/Editor/OpenGamePlayScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class LoadGamePlayScene
+{
+  static LoadGamePlayScene()
+  {
+    EditorSceneManager.newSceneCreated += OnNewScene;
+  }
+
+  private static void OnNewScene(Scene scene, UnityEditor.SceneManagement.NewSceneSetup setup, NewSceneMode mode)
+  {
+    if (scene.name != "GamePlay")
+    {
+      EditorSceneManager.OpenScene("Assets/Scenes/GamePlay.unity");
+    }
+  }
+}
